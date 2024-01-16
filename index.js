8922dea1f8f18bbba60da9dbcdb7b8416f328a07
Praneeth Saranga <praneethskulathilaka@gmail.com>
2024-01-16 13:01:17 +0530

    Moving backend files to parent folder

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const apiRoutes = require('./routes/apiRoutes');
+
+
+// Configurations
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+app.use(express.json());
+app.use(bodyParser.json({ extended:true }));
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(cors());
+
+// Api Routes
+app.use('/', apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
