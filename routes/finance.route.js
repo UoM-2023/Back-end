954ff61617b28a692925a284c69d3a6f02ed8be8
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-06-25 02:27:52 +0530

    edit residents form

--- routes/finance.route.js
+++ routes/finance.route.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const newFund = require('../controller/fundType.controller');
-const newExpense = require('../controller/expenses.controller');
-const newRevenue  = require('../controller/revenue.controller');
-const newPayment  = require('../controller/payments.controller');
-const { addNewUtility, getUtitlityDetails, getOneUtilityDetail } = require('../controller/utilityDetails.controller');
-const { addUtilityCharge, getUtilityCharges } = require('../controller/utilityCharges.controller');
-const { verifyToken, checkRole } = require('../middlewares/auth.middleware');
-const { getAUserCharge } = require('../controller/getAUserCharge.controller');
-
-
+const express = require("express");
+const newFund = require("../controller/fundType.controller");
+const newExpense = require("../controller/expenses.controller");
+const newRevenue = require("../controller/revenue.controller");
+const newPayment = require("../controller/payments.controller");
+const {
+  addNewUtility,
+  getUtitlityDetails,
+  getOneUtilityDetail,
+} = require("../controller/utilityDetails.controller");
+const {
+  addUtilityCharge,
+  getUtilityCharges,
+} = require("../controller/utilityCharges.controller");
+const { verifyToken, checkRole } = require("../middlewares/auth.middleware");
+const { getAUserCharge } = require("../controller/getAUserCharge.controller");
 
 const router = express.Router();
 
@@ -23,23 +28,31 @@ router.post("/utilityDetails", addNewUtility);
 router.post("/addUtilityUsage", addUtilityCharge);
 router.post("/getUtilityCharges", getUtilityCharges);
 
-router.post('/notify', (req, res) => {
-  console.log('Payment Notification:', req.body);
-  res.send('Payment notification received');
+router.post("/notify", (req, res) => {
+  console.log("Payment Notification:", req.body);
+  res.send("Payment notification received");
 });
 
 // Get Requests
-router.get('/editFunds', verifyToken, checkRole(['finance_manager','admin']), newFund.getAllFunds);
-router.get('/newExpense', verifyToken, checkRole(['finance_manager','admin']), newExpense.getAllExpenses);
-router.get('/revenue',newRevenue.getAllRevenues);
-router.get('/payment',newPayment.getAllPayments);
-router.get('/editFunds/:id', newFund.getAFund);
-router.get('/utilityDetails', getUtitlityDetails);
-router.get('/utilityDetails/:id', getOneUtilityDetail);
-router.get('/getUtilityCharges', getUtilityCharges);
-router.get('/getAUserCharge/:id', getAUserCharge);
-
-
+router.get(
+  "/editFunds",
+  verifyToken,
+  checkRole(["finance_manager", "admin"]),
+  newFund.getAllFunds
+);
+router.get(
+  "/newExpense",
+  verifyToken,
+  checkRole(["finance_manager", "admin"]),
+  newExpense.getAllExpenses
+);
+router.get("/revenue", newRevenue.getAllRevenues);
+router.get("/payment", newPayment.getAllPayments);
+router.get("/editFunds/:id", newFund.getAFund);
+router.get("/utilityDetails", getUtitlityDetails);
+router.get("/utilityDetails/:id", getOneUtilityDetail);
+router.get("/getUtilityCharges", getUtilityCharges);
+router.get("/getAUserCharge/:id", getAUserCharge);
 
 // Put Requests
 router.put("/editFunds/updateFund/:id", newFund.updateFund);
