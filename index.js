1f38ad61becaf0f467a742e8148b36c96e4f5645
Yasindu9 <144378880+Yasindu9@users.noreply.github.com>
2024-06-28 20:47:57 +0530

    final2.0

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -7,7 +7,6 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const config = require("./config/db.config");
 const database = require("./database/database");
-const http = require("http");
 const socketIo = require("socket.io");
 const socketManager = require("./sockets/socketManager");
 const multer = require("multer");
@@ -23,10 +22,9 @@ const authRoute = require("./routes/auth.route");
 const financeRoute = require("./routes/finance.route");
 const residentsdetailsRoute = require("./routes/residentsdetails.route");
 const staffdetailsRoute = require("./routes/staffdetails.route");
-const GuestDetailsRoute = require ("./routes/guest.route");
+const GuestDetailsRoute = require("./routes/guest.route");
 const ReservationsRoute = require("./routes/reservation.route");
 const FacilityRoute = require("./routes/facility.route");
-
 
 const userCredentialsRoute = require("./routes/usercredentials.route");
 const testRoute = require("./routes/test.route");
@@ -50,8 +48,20 @@ dotenv.config();
 app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000", methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"], credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 // Check database with dummy connections
@@ -71,13 +81,10 @@ app.use("/auth", authRoute);
 app.use("/finance", financeRoute);
 app.use("/residentsDetails", residentsdetailsRoute);
 app.use("/staffDetails", staffdetailsRoute);
-app.use("/GuestDetail",GuestDetailsRoute);
-app.use("/Reservation",ReservationsRoute);
-app.use("/Facility",FacilityRoute)
+app.use("/GuestDetail", GuestDetailsRoute);
+app.use("/Reservation", ReservationsRoute);
+app.use("/Facility", FacilityRoute);
 
-
-
-  
 app.use("/userCredentials", userCredentialsRoute);
 app.use("/testing", testRoute);
 app.use("/maintenance", maintenanceRoute);
